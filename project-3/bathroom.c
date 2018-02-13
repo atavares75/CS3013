9a0769cf649f1675e1a34099202ed153a6a277f7
Alex Tavares <amtavares@wpi.edu>
2018-02-12 21:40:48 -0500

    functions moved to bathroom.c

--- project-3/bathroom.c
+++ project-3/bathroom.c
@@ -1,3 +1,72 @@
 /*
   bathroom.c
+  Authors: Alex Tavares, Leo Gonslaves
+  Created On: 2/12/2018
 */
+#include "bathroom.h"
+
+typedef struct Bathroom_Object {
+	gender curGender;
+	int population;
+	pthread_mutex_t lock;
+	int queueLength;
+	pthread_cond_t empty;
+} Bathroom;
+
+
+extern Bathroom * const bathroom;
+
+
+/*
+ * Enter the bathroom, but wait until vacant if occupied
+ * by the opposite gender. Set state accordingly.
+ */
+void Enter(gender g) {
+	pthread_mutex_lock(&bathroom->lock);
+	//checks if bathroom is occupied by persons of the opposite gender
+	while (bathroom->population != 0 && bathroom->curGender != g) {
+		bathroom->queueLength++;
+		pthread_cond_wait(&bathroom->empty, &bathroom->lock);
+		bathroom->queueLength--;
+	}
+	bathroom->population++;
+	if (bathroom->population == 1) {
+		bathroom->curGender = g;
+	}
+	pthread_mutex_unlock(&bathroom->lock);
+
+
+}
+
+
+
+/*
+ * Leave bathroom. Set state to "vacant" if this thread is the
+ * last one out.
+ */
+void Leave() {
+	pthread_mutex_lock(&bathroom->lock);
+	bathroom->population--;
+	if (bathroom->population == 0) {
+		pthread_cond_broadcast(&bathroom->empty);
+	}
+	pthread_mutex_unlock(&bathroom->lock);
+}
+
+/*
+ * Initializes the bathroom. Should be only called by master thread.
+ */
+void Initialize() {
+	bathroom->curGender = MALE;
+	bathroom->population = 0;
+	bathroom->queueLength = 0;
+	pthread_mutex_init(&bathroom->lock, NULL);
+	pthread_cond_init(&bathroom->empty, NULL);
+}
+
+/*
+ *	Finalizes the execution of the bathroom.
+ */
+void Finalize(){
+
+}
--- project-3/bathroom.h
+++ project-3/bathroom.h
@@ -16,70 +16,26 @@ typedef enum Gender {
 	MALE, FEMALE
 } gender;
 
-typedef struct Bathroom_Object {
-	Gender curGender;
-	int population;
-	pthread_mutex_t lock;
-	int queueLength;
-	pthread_cond_t empty;
-} Bathroom;
-
-
-extern Bathroom * const bathroom;
-
-
 /*
  * Enter the bathroom, but wait until vacant if occupied
  * by the opposite gender. Set state accordingly.
  */
-void Enter(gender g) {
-	pthread_mutex_lock(&bathroom->lock);
-	//checks if bathroom is occupied by persons of the opposite gender
-	while (bathroom->population != 0 && bathroom->curGender != g) {
-		bathroom->queueLength++;
-		pthread_cond_wait(&bathroom->empty, &bathroom->lock);
-		bathroom->queueLength--;
-	}
-	bathroom->population++;
-	if (bathroom->population == 1) {
-		bathroom->curGender = g;
-	}
-	pthread_mutex_unlock(&bathroom->lock);
-
-
-}
-
-
+void Enter(gender g);
 
 /*
  * Leave bathroom. Set state to "vacant" if this thread is the
  * last one out.
  */
-void Leave() {
-	pthread_mutex_lock(&bathroom->lock);
-	bathroom->population--;
-	if (bathroom->population == 0) {
-		pthread_cond_broadcast(&bathroom->empty);
-	}
-	pthread_mutex_unlock(&bathroom->lock);
-}
+void Leave();
 
 /*
  * Initializes the bathroom. Should be only called by master thread.
  */
-void Initialize() {
-	bathroom->curGender = MALE;
-	bathroom->population = 0;
-	bathroom->queueLength = 0;
-	pthread_mutex_init(&bathroom->lock, NULL);
-	pthread_cond_init(&bathroom->empty, NULL);
-}
+void Initialize();
 
 /*
  *	Finalizes the execution of the bathroom.
  */
-void Finalize(){
-
-}
+void Finalize();
 
 #endif /* BATHROOM_H_ */
